583e33555fc19774a0e225eb3a73eed05efc80ca
Kaushik Dutta <kaushik-mac@Kaushiks-MacBook-Air.local>
2021-03-08 20:16:06 +0530

    Commit

---
+++ src/main/java/com/poc/cvtracker/CvTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.poc.cvtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CvTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/poc/cvtracker/CvTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poc.cvtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
